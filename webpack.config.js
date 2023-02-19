0222dfceb4c13c0c7d28ca963fa66cbbc2c5d2ff
arashi4ever <fxiao.mecheng@gmail.com>
2023-02-19 12:06:13 -0500

    Edit webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    page: './src/page.js',
   },
+  devtool: 'inline-source-map',
+ devServer: {
+   static: './dist',
+ },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+ optimization: {
+   runtimeChunk: 'single',
+ },
 };
-
